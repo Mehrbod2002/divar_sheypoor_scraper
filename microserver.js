39b7e308b0492aba76754b52317a3184bcbbe79f
Mehrbod Akhlaghpour <m9.akhlaghpoor@gmail.com>
2023-07-24 22:38:16 +0330

    feat: sheypoor

--- microserver.js
+++ microserver.js
@@ -7,6 +7,256 @@ const app = express();
 config();
 app.use(express.json());
 
+app.post("/sheypoor_send_sms", (req, res) => {
+    if (req.body.phone && req.body.phone.length == 10 && req.body.phone.substring(0, 2) == "91") {
+        fetch("https://www.sheypoor.com/api/protools/v2.1/auth/register", {
+            "credentials": "include",
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "fa",
+                "Content-Type": "application/json",
+                "X-Ticket": "null",
+                "protools-version": "2.1",
+                "protools-client-source": "web",
+                "protools-client-os": "web",
+                "protools-env": "production",
+                "source": "protools",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin"
+            },
+            "referrer": "https://www.sheypoor.com/pro/real-estate/login",
+            "body": JSON.stringify({ "cellphone": req.body.phone, "user_type": "real-estate" }),
+            "method": "POST",
+            "mode": "cors"
+        }).then(async (response) => {
+            if (response.status != 200 || !response.ok) {
+                res.json({ "status": "false", "message": "failed_to_send_sms" });
+            } else {
+                response = await response.json();
+                res.json({ "status": "true", "message": "sms_code_sent", "verify_token": response.token });
+            }
+        }).catch((_err) => {
+            res.json({ "status": "false", "message": "failed_to_send_sms" });
+        });
+    } else {
+        res.json({ "status": "false", "message": "invalid_phone" });
+    }
+});
+
+app.post("/sheypoor_verify_sms", (req, res) => {
+    if (req.body.phone && req.body.phone.length == 10 && req.body.phone.substring(0, 2) == "91" && req.body.code && req.body.code.length == 4 && /^\d+$/.test(req.body.code) && req.body.token) {
+        fetch("https://www.sheypoor.com/api/protools/v2.1/auth/authorize", {
+            "credentials": "include",
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "fa",
+                "Content-Type": "application/json",
+                "X-Ticket": "null",
+                "protools-version": "2.1",
+                "protools-client-source": "web",
+                "protools-client-os": "web",
+                "protools-env": "production",
+                "source": "protools",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin"
+            },
+            "referrer": "https://www.sheypoor.com/pro/real-estate/login",
+            "body": JSON.stringify({ "grant_type": "password", "username": req.body.token, "scope": "full", "password": req.body.code }),
+            "method": "POST",
+            "mode": "cors"
+        }).then(async (response) => {
+            if (response.status != 200 || !response.ok) {
+                res.json({ "status": "false", "message": "invalid_phone_and_code" });
+            } else {
+                response = await response.json();
+                res.json({ "status": "true", "message": "done", "token": response.access_token, "expire": response.expires_in + new Date().getTime() });
+            }
+        }).catch((_err) => {
+            res.json({ "status": "false", "message": "invalid_phone_and_code" });
+        });
+    } else {
+        res.json({ "status": "false", "message": "invalid_phone_and_code" });
+    }
+});
+
+app.post("/sheypoor_post", (req, res) => {
+    const url = 'https://www.sheypoor.com/api/protools/v2.1/listings';
+    const headers = {
+        'Host': 'www.sheypoor.com',
+        'Content-Length': 0,
+        'Sec-Ch-Ua': '"Not A(Brand";v="24", "Chromium";v="110"',
+        'Protools-Client-Source': 'web',
+        'Accept-Language': 'fa',
+        'Source': 'protools',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Protools-Version': '2.1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json, text/plain, */*',
+        'X-Ticket': 'b9beb60260f5c05ba1d8f3efdbb38999',
+        'Protools-Env': 'production',
+        'Protools-Client-Os': 'web',
+        'Sec-Ch-Ua-Platform': '"Linux"',
+        'Origin': 'https://www.sheypoor.com',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://www.sheypoor.com/pro/real-estate/file/new',
+        'Accept-Encoding': 'gzip, deflat',
+        'Cookie': `verify_token=Bearer+${req.body.token}`,
+    }
+    const category = {
+        "خرید و فروش ویلا": 44586, "رهن و اجاره خانه و آپارتمان": 43606, "خرید و فروش خانه و آپارتمان": 43604,
+        "اجاره کوتاه مدت ویلا، سوئیت": 44585, "رهن و اجاره اداری، تجاری و صنعتی": 43607, "خرید و فروش اداری، تجاری و صنعتی": 43605,
+        "زمین و باغ": 44099
+    }
+    var years = {
+        "1370": "455230",
+        "1371": "455229",
+        "1372": "455228",
+        "1373": "455227",
+        "1374": "455226",
+        "1375": "455225",
+        "1376": "455224",
+        "1377": "455223",
+        "1378": "455222",
+        "1379": "455221",
+        "1380": "455220",
+        "1381": "455219",
+        "1382": "455218",
+        "1383": "455217",
+        "1384": "455216",
+        "1385": "455215",
+        "1386": "455214",
+        "1387": "455213",
+        "1388": "455212",
+        "1389": "455211",
+        "1390": "455210",
+        "1391": "455209",
+        "1392": "455208",
+        "1393": "455207",
+        "1394": "455206",
+        "1395": "455205",
+        "1396": "455204",
+        "1397": "455203",
+        "1398": "455202",
+        "1399": "455201",
+        "1400": "455200",
+        "1401": "458100",
+        "1402": "459400",
+        "قبل از 1370": "455250"
+    }
+    var built_year = 0;
+    if (req.body.year && req.body.year > 1402) {
+        res.json({ "status": "false", "message": "invalid_year" });
+        return;
+    }
+    if (req.body.year < 1370) {
+        built_year = years["قبل از 1370"];
+    } else {
+        built_year = years[req.body.year];
+    }
+    if (built_year == 0) {
+        res.json({ "status": "false", "message": "invalid_year" });
+        return;
+    }
+    var rooms = {
+        "1": "439414",
+        "2": "439415",
+        "3": "439416",
+        "4": "439417",
+        "بدون اتاق": "439837",
+        "5 به بالا": "439418"
+    };
+    var ava_room = 0;
+    if (req.body.rooms < 0) {
+        res.json({ "status": "false", "message": "invalid_room" });
+    }
+    if (req.body.rooms == 0) {
+        ava_room = rooms["بدون اتاق"];
+    } else if (req.body.rooms >= 5) {
+        ava_room = rooms["5 به بالا"];
+    } else {
+        ava_room = rooms[req.body.rooms];
+    }
+    var city_cat = {
+        "تهران": "8",
+        "مازندران": "27",
+        "آذربایجان شرقی": "1",
+        "آذربایجان غربی": "2",
+        "اردبیل": "3",
+        "اصفهان": "4",
+        "البرز": "5",
+        "ایلام": "6",
+        "بوشهر": "7",
+        "چهارمحال و بختیاری": "9",
+        "خراسان جنوبی": "10",
+        "خراسان رضوی": "11",
+        "خراسان شمالی": "12",
+        "خوزستان": "13",
+        "زنجان": "14",
+        "سمنان": "15",
+        "سیستان و بلوچستان": "16",
+        "فارس": "17",
+        "قزوین": "18",
+        "قم": "19",
+        "کردستان": "20",
+        "کرمان": "21",
+        "کرمانشاه": "22",
+        "کهگیلویه و بویراحمد": "23",
+        "گلستان": "24",
+        "لرستان": "25",
+        "گیلان": "26",
+        "مرکزی": "28",
+        "هرمزگان": "29",
+        "همدان": "30",
+        "یزد": "31"
+    }
+    var selected_city = null;
+    if (req.body.city && city_cat.indexOf(req.body.city) != -1) {
+        selected_city = req.body.city;
+    } else {
+        res.json({ "status": "false", "message": "invalid_city" });
+    }
+    const data = {
+        location: selected_city,
+        a96010: built_year,
+        a96002: req.body.parking,
+        a96003: req.body.warehouse,
+        a96004: req.body.elevator,
+        a68085: req.body.size,
+        a68133: ava_room,
+        price: req.body.price,
+        description: req.body.description,
+        images: [],
+        videos: [],
+        category: category[req.body.category],
+    };
+    headers['Content-Length'] = data.length;
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+        credentials: 'include',
+        referrer: 'https://www.sheypoor.com/pro/real-estate/login',
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.errors) {
+                res.json({ "status": "false", "message": "invalid_request", "errors": data.errors });
+            } else {
+                if (data.message && data.message == "آگهی با موفقیت ثبت شد.") {
+                    res.json({ "status": "true", "message": "posted" });
+                }
+            }
+        })
+        .catch((error) => console.error('Error:', error));
+});
+
 app.post("/divar_send_sms", (req, res) => {
     const options = {
         url: 'https://api.divar.ir/v5/auth/authenticate',
@@ -260,5 +510,5 @@ app.post("/divar_post", (req, res) => {
         });
 });
 
-app.listen(port, process.env.PORT);
+app.listen(process.env.PORT);
 
--- test.py
+++ test.py
@@ -1,41 +1,53 @@
 import requests 
 
-#################################### Divar 
+# #################################### Divar 
+# ##### Verify user
+# res = requests.post("http://127.0.0.1:3000/divar_send_sms", json = { "phone" : "9138780275"}).json()
+# if res["status"] == 'true':
+#     code = input("code: ")
+#     res = requests.post("http://127.0.0.1:3000/divar_verify_sms", json = { "phone" : "9138780275","code": code})
+#     print(res.text)
+#     # { status : "true" or "false" , "token": "....."}
+
+
+# ######### Post
+# requested = {
+#     "token": "",
+#     "city": "اصفهان",
+#     "category": "apartment-rent", # apartment-sell
+#     "images": [],
+#     "size": 2555,
+#     "user_type": "شخصی", # مشاور املاک
+#     "rent_to_single": "true",
+#     "rooms": "چهار",
+#     "year": "۱۳۹۴",
+#     "floor": "0",
+#     "elevator": "false",
+#     "parking": "true",
+#     "warehouse": "true",
+#     "national_id": "1273746643",
+#     "title": "ملک برای فروش",
+#     "description": "مناسب و مجهز",
+# }
+# res = requests.post("http://127.0.0.1:3000/divar_post", json = requested)
+# # { "status": "true", "message": "posted" }
+# print(res.text)
+
+
+# ################ Cities
+# res = requests.get("http://127.0.0.1:3000/divar_cities").json()
+# print(res)
+
+
+################## Sheypoor
 ##### Verify user
-res = requests.post("http://127.0.0.1:3000/divar_send_sms", json = { "phone" : "9138780275"}).json()
-if res["status"] == 'true':
-    code = input("code: ")
-    res = requests.post("http://127.0.0.1:3000/divar_verify_sms", json = { "phone" : "9138780275","code": code})
-    print(res.text)
+# res = requests.post("http://127.0.0.1:3000/sheypoor_send_sms", json = { "phone" : "9138780275"}).json()
+# print(res)
+# if res["status"] == 'true':
+#     code = input("code: ")
+#     token = input("token: ")
+#     res = requests.post("http://127.0.0.1:3000/sheypoor_verify_sms", json = { "phone" : "9138780275","code": code, "token": token})
+#     print(res.text)
     # { status : "true" or "false" , "token": "....."}
-
-
-######### Post
-requested = {
-    "token": "",
-    "city": "اصفهان",
-    "category": "apartment-rent", # apartment-sell
-    "images": [],
-    "size": 2555,
-    "user_type": "شخصی", # مشاور املاک
-    "rent_to_single": "true",
-    "rooms": "چهار",
-    "year": "۱۳۹۴",
-    "floor": "0",
-    "elevator": "false",
-    "parking": "true",
-    "warehouse": "true",
-    "national_id": "1273746643",
-    "title": "ملک برای فروش",
-    "description": "مناسب و مجهز",
-}
-res = requests.post("http://127.0.0.1:3000/divar_post", json = requested)
-# { "status": "true", "message": "posted" }
-print(res.text)
-
-
-################ Cities
-res = requests.get("http://127.0.0.1:3000/divar_cities").json()
-print(res)
-
-
+# res = requests.post("http://127.0.0.1:3000/sheypoor_post", json = { "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInR5cGUiOiJWRVJJRlkifQ.eyJqdGkiOiI2NGJlYmE4YmJjOWRkMC4xMDM0MTYxOCIsImlzcyI6InNoZXlwb29yIiwiYXVkIjoic2hleXBvb3IiLCJpYXQiOjE2OTAyMjExOTUsImV4cCI6MTY5MDIyMTc5NSwibmJmIjoxNjkwMjIxMTk1LCJtb2JpbGUiOiIwOTEzODc4MDI3NSIsInVzZXJJZCI6IjE3Nzg4MjMifQ.YsyHire2fachoSIcoG940QetHanEkesVRCU30-p-1o8"}).json()
+# print(res)
